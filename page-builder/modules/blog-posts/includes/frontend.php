281e20438db0733180bd4ccfe85a4387b9939440
Faisal Khurshid <imfaisalkh@gmail.com>
2018-09-17 06:17:40 +0500

    Implemented new blog layout.

--- page-builder/modules/blog-posts/includes/frontend.php
+++ page-builder/modules/blog-posts/includes/frontend.php
@@ -2,9 +2,10 @@
 
     // WP_QUERY Arguments
     $blog_args = array(
-      'post_type'     => 'post',
-      'posts_per_page'    => $settings->count,
+      'post_type'       => 'post',
+      'posts_per_page'  => $settings->count,
       'tax_query'       => array(),
+      'paged'           => get_query_var('page') ? get_query_var('page') : 1, // https://wordpress.stackexchange.com/questions/180784/what-is-the-difference-between-paged-and-page
     );
 
     // Check if category(ies) is/are defined, if yes, inject it in 'tax_query' array.
@@ -27,12 +28,25 @@
 ?>
 
 <div class="module-wrapper posts-grid <?php echo $wrapper_class; ?>" data-layout="<?php echo $settings->style; ?>">
-  <?php if ( $blog_query->have_posts() ) : while ( $blog_query->have_posts() ) : $blog_query->the_post(); ?>
-    <?php get_template_part( 'content', get_post_format() ); ?>
-  <?php endwhile; else: ?>
-    <p>No posts found.</p>
-  <?php endif; ?>
-  <?php wp_reset_postdata(); ?>
+  <?php
+    if ( $blog_query->have_posts() ) :
+      $counter = 1;
+      while( $blog_query->have_posts() ) : $blog_query->the_post();
+        include( locate_template( 'content-post.php' ) ); 
+        $counter++;
+      endwhile;
+    else:
+      get_template_part( 'content', 'none' );
+    endif;
+  ?>	
 </div>
 
-<div class="load-more"><a href="#">Load More</a></p>
+<?php if ( !(get_query_var('page') >= $blog_query->max_num_pages) ) { ?> 
+  <div class="load-more"><?php echo get_next_posts_link( esc_html__('Load More', 'capstone'), $blog_query->max_num_pages ); ?></div>
+  <div class="page-load-status">
+    <p class="infinite-scroll-last"><?php esc_html_e('End of content', 'capstone'); ?></p>
+    <p class="infinite-scroll-error"><?php esc_html_e('No more pages to load', 'capstone'); ?></p>
+  </div>
+<?php } ?>
+
+<?php wp_reset_postdata(); ?>
