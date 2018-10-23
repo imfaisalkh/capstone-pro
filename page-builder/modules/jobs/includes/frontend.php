d99a1cbd27e066d9e435b94c3f64cb054dc8538c
Faisal Khurshid <imfaisalkh@gmail.com>
2018-10-23 14:36:09 +0500

    Improved 'Job Listing' beaver builder module.
    Signed-off-by: Faisal Khurshid <imfaisalkh@gmail.com>

--- page-builder/modules/jobs/includes/frontend.php
+++ page-builder/modules/jobs/includes/frontend.php
@@ -4,5 +4,5 @@
 // var_dump('[jobs '. $count .']');
 ?>
 <main class="module-wrapper jobs-list">
-  <?php echo '[jobs per_page="'. $settings->count .'"]'; ?>
+  <?php echo '[jobs per_page="'. $settings->count .'" orderby="'. $settings->order_by .'" order="'. $settings->order .'" show_filters="false" categories="'. $settings->categories .'" job_types="'. $settings->job_types .'" location="'. $settings->location .'" keywords="'. $settings->keywords .'" post_status="'. $settings->post_status .'" show_pagination="'. $settings->show_pagination .'" ]'; ?>
 </main>
--- page-builder/modules/jobs/jobs.php
+++ page-builder/modules/jobs/jobs.php
@@ -31,6 +31,77 @@ FLBuilder::register_module( 'JobListingClass', array(
                         'description'   => 'How many jobs to list.',
                         'default'       => '-1',
                     ),
+                    'order_by' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Order By', 'fl-builder' ),
+                        'default'       => 'featured',
+                        'options'       => array(
+                            'featured'      => __( 'Featured', 'fl-builder' ),
+                            'title'      => __( 'Title', 'fl-builder' ),
+                            'ID'      => __( 'ID', 'fl-builder' ),
+                            'name'      => __( 'Name', 'fl-builder' ),
+                            'date'      => __( 'Date', 'fl-builder' ),
+                            'modified'      => __( 'Date Modified', 'fl-builder' ),
+                            'rand'      => __( 'Random', 'fl-builder' )
+                        )
+                    ),
+                    'order' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Order', 'fl-builder' ),
+                        'default'       => 'desc',
+                        'options'       => array(
+                            'desc'      => __( 'Descending', 'fl-builder' ),
+                            'asc'      => __( 'Ascending', 'fl-builder' )
+                        )
+                    ),
+                    'categories'     => array(
+                        'type'          => 'text',
+                        'label'         => __( 'Categories', 'fl-builder' ),
+                        'description'   => 'A comma separate slugs to limit the jobs to certain categories.',
+                    ),
+                    'job_types'     => array(
+                        'type'          => 'text',
+                        'label'         => __( 'Job Types', 'fl-builder' ),
+                        'description'   => 'A comma separate slugs to limit the jobs to certain job types.',
+                    ),
+                    'location'     => array(
+                        'type'          => 'text',
+                        'label'         => __( 'Location', 'fl-builder' ),
+                        'description'   => 'Enter a location keyword to search by default.',
+                    ),
+                    'keywords'     => array(
+                        'type'          => 'text',
+                        'label'         => __( 'Keywrod', 'fl-builder' ),
+                        'description'   => 'Enter a keyword to search by default.',
+                    ),
+                    'post_status' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Status', 'fl-builder' ),
+                        'default'       => 'publish',
+                        'options'       => array(
+                            'publish'      => __( 'Active Jobs', 'fl-builder' ),
+                            'expired'      => __( 'Expired Jobs', 'fl-builder' )
+                        )
+                    ),
+                    'featured' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Featured Jobs', 'fl-builder' ),
+                        'default'       => '',
+                        'options'       => array(
+                            ''      => __( 'Show BOTH Featured + Non-Featured Jobs', 'fl-builder' ),
+                            'true'      => __( 'Show Featured Jobs ONLY', 'fl-builder' ),
+                            'false'      => __( 'Show Non-Featured Jobs ONLY', 'fl-builder' )
+                        )
+                    ),
+                    'show_pagination' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Pagination', 'fl-builder' ),
+                        'default'       => 'false',
+                        'options'       => array(
+                            'true'      => __( 'Show Numbered Pagination', 'fl-builder' ),
+                            'false'      => __( 'Show Load More Button', 'fl-builder' )
+                        )
+                    ),
                 )
             )
         )
