dff537a3e236f91601df8d47c753addf1f48dae7
Faisal Khurshid <imfaisalkh@gmail.com>
2018-11-06 18:19:02 +0500

    Signed-off-by: Faisal Khurshid <imfaisalkh@gmail.com>

--- filters/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#-------------------------------------------------------------------------------#
-#  WP Job Manager - Remove Deadline Markup from Listing
-#-------------------------------------------------------------------------------#
-        
-    global $job_manager_application_deadline;
-    remove_filter( 'job_listing_meta_end', array( $job_manager_application_deadline, 'display_the_deadline' ));
