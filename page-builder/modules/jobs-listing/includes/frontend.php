746ea0d170e73541f4623076ba3e6789a03be96b
Muhammad Faisal <imfaisalkh@gmail.com>
2019-03-06 05:23:01 +0500

    Fixed "jobs" module not showing "featured" jobs issue.

--- page-builder/modules/jobs-listing/includes/frontend.php
+++ page-builder/modules/jobs-listing/includes/frontend.php
@@ -1,8 +1,7 @@
 <?php
 // Helper Variable(s)
-// $count = $settings->count ? $settings->count : '';
-// var_dump($settings);
+$featured_param = $settings->featured ? " featured='{$settings->featured}' " : "";
 ?>
 <section class="module-wrapper jobs-list">
-  <?php echo '[jobs per_page="'. $settings->count .'" orderby="'. $settings->order_by .'" order="'. $settings->order .'" show_filters="false" categories="'. $settings->categories .'" job_types="'. $settings->job_types .'" location="'. $settings->location .'" keywords="'. $settings->keywords .'" post_status="'. $settings->post_status .'" featured="'. $settings->featured .'" show_pagination="'. $settings->show_pagination .'" ]'; ?>
+  <?php echo "[jobs per_page='{$settings->count}' orderby='{$settings->order_by}' order='{$settings->order}' show_filters='false' categories='{$settings->categories}' job_types='{$settings->job_types}' {$featured_param} location='{$settings->location}' keywords='{$settings->keywords}' post_status='{$settings->post_status}' show_pagination='{$settings->show_pagination}']"; ?>
 </section>
